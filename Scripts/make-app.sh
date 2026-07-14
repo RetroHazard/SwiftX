734c1cd2498ca1f44006e2662a8f63b3ed42071c
RetroHazard <Griever92@gmail.com>
2026-07-14 12:07:48 -0600

    style: Put each copyright notice on its own line in About

--- Scripts/make-app.sh
+++ Scripts/make-app.sh
@@ -53,7 +53,8 @@ cat > "$APP/Contents/Info.plist" <<PLIST
     <key>LSMinimumSystemVersion</key>
     <string>13.0</string>
     <key>NSHumanReadableCopyright</key>
-    <string>Copyright © 2026 RetroHazard. Based on ShareX © 2007-2026 ShareX Team. Licensed under GPL v3.</string>
+    <!-- &#10; = newline: each notice renders on its own line in About -->
+    <string>Copyright © 2026 RetroHazard.&#10;Based on ShareX © 2007-2026 ShareX Team.&#10;Licensed under GPL v3.</string>
     <key>LSUIElement</key>
     <true/>
     <key>NSPrincipalClass</key>
